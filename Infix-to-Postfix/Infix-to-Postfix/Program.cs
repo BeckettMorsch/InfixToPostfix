cc90d0c9299ed24f9eb4f6e156c6948c66535a81
BeckettMorsch <beckettmorsch@gmail.com>
2018-01-12 12:57:35 -0500

    stuff

--- Infix-to-Postfix/Infix-to-Postfix/Program.cs
+++ Infix-to-Postfix/Infix-to-Postfix/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Infix_to_Postfix
 {
@@ -10,10 +11,56 @@ namespace Infix_to_Postfix
     {
         static void Main(string[] args)
         {
-            //s/
+            Console.WriteLine("Enter in an expression: ");
+            String xp = Console.ReadLine();
+
+            String[] letters = new String[xp.Length];
+            for(int i=0;i<letters.Length;i++)
+            {
+                letters[i] = xp.Substring(i, 1);
+            }
+
+            Stack st = new Stack();
 
 
 
+
+
+
+
+
+
+            /*/
+            st.Push('A');
+
+            Console.WriteLine("Current stack: ");
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }
+
+            Console.WriteLine();
+
+            st.Push('V');
+            Console.WriteLine("The next poppable value in stack: {0}", st.Peek());
+            Console.WriteLine("Current stack: ");
+
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Removing values ");
+            st.Pop();
+
+            Console.WriteLine("Current stack: ");
+            foreach (char c in st)
+            {
+                Console.Write(c + " ");
+            }/*/
         }
     }
-}
+} 
